3fa9a656568ec2e1f855b13e2177bb8d2f48a7d4
taxiicommit <merssage123@yahoo.co.jp>
2016-11-20 21:49:43 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
      # ユーザーログイン後にユーザー情報のページにリダイレクトする
      log_in user
      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-     redirect_to user  # Railsでは、user_url(user)にコードを変換して、ユーザープロフィールページへのルーティングします。
+     redirect_back_or user  # Railsでは、user_url(user)にコードを変換して、ユーザープロフィールページへのルーティングします。
    else
      flash.now[:danger] = 'Invalid email/password combination'
      render 'new'
