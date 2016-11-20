cd2c440baaa3adc785954e7cc0463c5d009abea2
taxiicommit <merssage123@yahoo.co.jp>
2016-11-20 17:58:08 +0900

    Finish log in/log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+   if user && user.authenticate(params[:session][:password])
+     # ユーザーログイン後にユーザー情報のページにリダイレクトする
+     log_in user
+     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+     redirect_to user  # Railsでは、user_url(user)にコードを変換して、ユーザープロフィールページへのルーティングします。
+   else
+     flash.now[:danger] = 'Invalid email/password combination'
+     render 'new'
+   end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
